dd33b7683bed62066a62474280c505cf9942252d
Sofia Victoria <tarabusi.sv@gmail.com>
2023-11-28 22:42:43 -0300

    importo jsonfy, request, CORS, SQLAlchemy, Marsh..

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+# Importa las clases Flask, jsonify y request del módulo flask
+from flask import Flask, jsonify, request
+# Importa la clase CORS del módulo flask_cors
+from flask_cors import CORS
+# Importa la clase SQLAlchemy del módulo flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+# Importa la clase Marshmallow del módulo flask_marshmallow
+from flask_marshmallow import Marshmallow
+
+
 
 app = Flask(__name__) #__name__ es el nombre del archivo, guardo este objeto en la variable app
 
